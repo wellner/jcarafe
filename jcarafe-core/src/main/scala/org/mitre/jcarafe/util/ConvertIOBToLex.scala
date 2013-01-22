4cc4406e0557aafeb307e136aa28a838b0995cf2
Ben Wellner <wellner@mitre.org>
2013-01-22 14:37:54 -0500

    Minor change to utility that converts IOB formatted data to lex-encoded tokens with attributes

--- jcarafe-core/src/main/scala/org/mitre/jcarafe/util/ConvertIOBToLex.scala
+++ jcarafe-core/src/main/scala/org/mitre/jcarafe/util/ConvertIOBToLex.scala
@@ -17,12 +17,13 @@ object ConvertIOBToLex {
     case "COMMA" | "," | "$" | "." | "''" | "``" | ":" | "(" | ")" | "`" | "'" | "#" => "PUNCT"
     case a => a
   }
+  
+  val commaReg = "COMMA".r
 
   def mapObs(obs: String) = obs match {
-    case "COMMA" => ","
     case "``" => "\""
     case "''" => "\""
-    case a => a
+    case a => commaReg.replaceAllIn(a,",")
   }
 
   def writeTok(os: Writer, pos: String, obs: String) = {
@@ -35,47 +36,78 @@ object ConvertIOBToLex {
     os.write("</lex>")
   }
 
-  def getLineInfoStd(lineRx: Regex)(line: String) : (String,String,String) = {
-    val lineRx(lab,pos,obs) = line
-    (lab,pos,obs)
+  def getLineInfoStd(lineRx: Regex)(line: String): (String, String, String) = {
+    val lineRx(lab, pos, obs) = line
+    (lab, pos, obs)
   }
 
-  def getLineInfoMayo(lineRx: Regex)(line: String) : (String,String,String) = {
-    val lineRx(obs,pos,lab) = line
-    (lab,pos,obs)
+  def getLineInfoMayo(lineRx: Regex)(line: String): (String, String, String) = {
+    val lineRx(obs, pos, lab) = line
+    (lab, pos, obs)
   }
-  
-  def main (args: Array[String]) = {
+
+  def processNPChunkingData(lines: Iterator[String], os: java.io.OutputStreamWriter) = {
+    var inside: Option[String] = None
+    val lineFn = getLineInfoStd(""" \d+[ ]+\d+[ ]+(\S+)[ ]+(\S+)[ ]+(\S+).*""".r) _
+    lines foreach { line =>
+      if (!((line.length < 1) || (line(0) == '#'))) {
+        val (lab, pos, obs) = lineFn(line)
+        if (lab.startsWith("O")) {
+          inside match { case Some(v) => os.write("</" + v + ">") case None => }
+          writeTok(os, pos, obs)
+          inside = None
+        } else {
+          val sp = lab.split('-')
+          if (sp(0) == "B") {
+            inside match { case Some(v) => os.write("</" + v + ">") case None => }
+            os.write("<" + sp(1) + ">")
+            writeTok(os, pos, obs)
+            inside = Some(sp(1))
+          } else {
+            writeTok(os, pos, obs)
+          }
+        }
+      } else if (line.length < 1) {
+        os.write("\n\n\n")
+      }
+    }
+    os.flush
+    os.close
+  }
+
+  def main(args: Array[String]) = {
     val src = io.Source.fromFile(new java.io.File(args(0)))("UTF-8")
     val ostr = new java.io.FileOutputStream(new java.io.File(args(1)))
     val std = ((args.length < 3) || (args(2) == "std"))
+    val tbMode = ((args.length > 2) && (args(2) equals "tb"))
     val os = new java.io.OutputStreamWriter(new java.io.BufferedOutputStream(ostr), "UTF-8")
-    var lines = src.getLines() 
-    var inside : Option[String] = None
-    val lineFn = if (std) getLineInfoStd(""" \d+[ ]+\d+[ ]+(\S+)[ ]+(\S+)[ ]+(\S+).*""".r) _ else getLineInfoMayo("""(\S+) (\S+) (\S+)""".r) _ 
+    var lines = src.getLines()
+    var inside: Option[String] = None
+    val lineFn =
+      if (std) getLineInfoStd(""" \d+[ ]+\d+[ ]+(\S+)[ ]+(\S+)[ ]+(\S+).*""".r) _
+      else getLineInfoMayo("""(\S+) (\S+) (\S+)""".r) _
     lines foreach { line =>
       if (!((line.length < 1) || (line(0) == '#'))) {
-	val (lab,pos,obs) = lineFn(line)
-	if (lab == "O") {
-	  inside match {case Some(v) => os.write("</"+v+">") case None => }
-	  writeTok(os,pos,obs)
-	  inside = None
-	}
-	else {
-	  val sp = lab.split('-')
-	  if (sp(0) == "B") {
-	    inside match {case Some(v) => os.write("</"+v+">") case None => }
-	    os.write("<"+sp(1)+">")
-	    writeTok(os,pos,obs)
-	    inside = Some(sp(1))
-	  } else {
-	    writeTok(os,pos,obs)
-	  }
-	}
+        val (lab, pos, obs) = lineFn(line)
+        if (lab == "O") {
+          inside match { case Some(v) => os.write("</" + v + ">") case None => }
+          writeTok(os, pos, obs)
+          inside = None
+        } else {
+          val sp = lab.split('-')
+          if (sp(0) == "B") {
+            inside match { case Some(v) => os.write("</" + v + ">") case None => }
+            os.write("<" + sp(1) + ">")
+            writeTok(os, pos, obs)
+            inside = Some(sp(1))
+          } else {
+            writeTok(os, pos, obs)
+          }
+        }
       } else if (line.length < 1) {
-	os.write("\n\n")
+        os.write("\n\n")
       }
-		 }
+    }
     os.flush
     os.close
     ostr.close
