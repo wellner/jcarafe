6297b52990efbc575b7fdcaa7976ad68bf989e88
Ben Wellner <wellner@mitre.org>
2013-03-27 10:51:06 -0400

    Updated Zh mapping process

--- jcarafe-core/src/main/scala/org/mitre/jcarafe/util/MapZhTreeBankToUniversal.scala
+++ jcarafe-core/src/main/scala/org/mitre/jcarafe/util/MapZhTreeBankToUniversal.scala
@@ -2,7 +2,7 @@ package org.mitre.jcarafe.util
 
 import java.io.{File,Writer,PrintWriter,OutputStream}
 
-class MapZhTreeBankToUniversal {
+class MapZhTreeBankToUniversal(val posMapping: Map[String,String]) {
   
   def handleLine(line: String) = {
     val sbuf = new StringBuilder
@@ -10,12 +10,13 @@ class MapZhTreeBankToUniversal {
       tok.split('_').toList match {
         case wd :: pos :: Nil =>
           sbuf append "<lex pos=\""
-          sbuf append pos
+          sbuf append posMapping.get(pos).getOrElse("NOUN")
           sbuf append "\">"
           sbuf append wd
           sbuf append "</lex>"
         case t => sbuf append tok 
       }
+      sbuf append ' '
     }
     sbuf.toString
   }
@@ -30,10 +31,24 @@ class MapZhTreeBankToUniversal {
 
 object MapZhTreeBankToUniversal {
   
+  def loadMapping(f: File) = {
+    var mp = Map[String,String]()
+    io.Source.fromFile(f).getLines foreach { l =>
+      l.split('\t').toList match {
+        case o :: m :: Nil => mp += (o -> m)
+        case _ =>
+      }
+    }
+    mp
+  }
+  
   def main(args: Array[String]) = {
-    val mapper = new MapZhTreeBankToUniversal
+    if (args.length < 3) throw new RuntimeException("Insufficient number of arguments")
     val idir = new File(args(0))
     val odir = new File(args(1))
+    val mappingFile = new File(args(2))
+    val mapper = new MapZhTreeBankToUniversal(loadMapping(mappingFile))
+
     idir.listFiles foreach {f =>
       println("Processing file: " + f)
       val ofile = new File(odir.getPath + "/" + f.getName())
