4bfd2b2ebb738cb1c994b05a7ea8fee8c7391252
Ben Wellner <wellner@mitre.org>
2013-07-12 14:47:41 -0400

    Added convex test 1

---
+++ jcarafe-core/src/test/scala/org/mitre/jcarafe/optimize/ConvexOptimizerSpec.scala
@@ -0,0 +1,25 @@
+package org.mitre.jcarafe.optimize
+
+import org.scalatest.Spec
+
+class ConvexOptimizerSpec extends Spec {
+
+  describe("Optimizer test 1") {
+    it("Mean test") {
+      val target = 3
+      val fn = new FunctionEvaluation {
+        def evaluate(x: Array[Double], gradient: Array[Double], n: Int, step: Double) : Double = {
+          val resid = x(0) - target
+          val sqloss = math.pow(resid,2.0)
+          gradient(0) = resid
+          sqloss
+        }
+      }
+      val p = new Params
+      val sol = Array(0.0)
+      val optimizer = new LbfgsOptimizer(sol, fn, p)
+      optimizer.optimize()
+      println("-- Mean Test Solution: " + sol(0))
+    }
+  }
+}
