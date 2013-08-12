58944648713ab7008fe80d58d2877587a044372b
Ben Wellner <wellner@mitre.org>
2013-08-12 12:18:03 -0400

    Updated MAT-JSON reader to accept MAT 2.0 annotations

--- jcarafe-core/src/main/scala/org/mitre/jcarafe/util/JsonAnnotationHandler.scala
+++ jcarafe-core/src/main/scala/org/mitre/jcarafe/util/JsonAnnotationHandler.scala
@@ -6,38 +6,56 @@ package org.mitre.jcarafe.util
 object JsonAnnotationHandler {
 
   /*
+   * This handles getting the attribute labels whether using MAT v < 2.0 or the
+   * newer MAT 2.0 or > schema.
+   */
+  def getAttributeLabels(attrList: List[JsonType]): List[String] = {
+    var atts: List[String] = Nil
+    attrList foreach { jobj =>
+      jobj match {
+        case JsString(s) => atts = s :: atts // old schema - all strings here are the attribute values
+        case JsObject(obj) => // NEW scheme - objects here indicate name of attribute value and attribute type (which is ignored here)
+          obj.get("name") foreach { case JsString(s) => atts = s :: atts case _ => }
+        case _ =>
+      }
+    }
+    atts
+  }
+  /*
    * This gathers annotations over the MAT-JSON "defined" schema.
    */
   def getAnnotations(signal: Option[String], js: JsonType, tagset: Tagset, asPreProc: Boolean = false, justLabel: Boolean = false, toks: Boolean = false): List[Annotation] =
     js match {
       case JsArray(arr) =>
-        arr.filter {
-          case JsObject(o) =>
-            val s = o("type") match { case JsString(s) => s case _ => "" }
-            val hasId = o.get("hasID") match { case Some(JsTrue) => true case _ => false }
-            val hasSpan = o.get("hasSpan") match { case Some(JsFalse) => false case _ => true }
-            o("attrs") match {
-              case JsArray(ar_p) =>
-                val ar = ar_p map { case JsString(s) => s case _ => "--" }
-                if (ar.length > 0) {
-                  tagset.set.exists { al =>
-                    al match {
-                      case Label(l, avs) =>
-                        val t = (l == s && (justLabel || avs.forall { case (k, v) => ar.contains(k) }))
-                        t
-                      case SLabel(l) if ((l == s)) => true // select annotations ignoring attributes
-                      case _ => false
+        val asetsOfTargetType =
+          arr.filter {
+            case JsObject(o) =>
+              val s = o("type") match { case JsString(s) => s case _ => "" }
+              val hasId = o.get("hasID") match { case Some(JsTrue) => true case _ => false }
+              val hasSpan = o.get("hasSpan") match { case Some(JsFalse) => false case _ => true }
+              o("attrs") match {
+                case JsArray(ar_p) =>
+                  val ar = getAttributeLabels(ar_p)
+                  if (ar.length > 0) {
+                    tagset.set.exists { al =>
+                      al match {
+                        case Label(l, avs) =>
+                          val t = (l == s && (justLabel || avs.forall { case (k, v) => ar.contains(k) }))
+                          t
+                        case SLabel(l) if ((l == s)) => true // select annotations ignoring attributes
+                        case _ => false
+                      }
                     }
-                  }
-                } else tagset.set.exists { al => al match { case SLabel(l) => l == s case _ => false } }
-              case _ => tagset.set.exists { al => al match { case SLabel(l) => l == s case _ => false } }
-            }
-          case _ => false
-        } flatMap { el =>
+                  } else tagset.set.exists { al => al match { case SLabel(l) => l == s case _ => false } }
+                case _ => tagset.set.exists { al => al match { case SLabel(l) => l == s case _ => false } }
+              }
+            case _ => false
+          }
+        asetsOfTargetType flatMap { el =>
           el match {
             case JsObject(o) =>
               val s = o("type") match { case JsString(s) => s case a => throw new RuntimeException("No valid type value: " + a) }
-              val s_attsKeys = o("attrs") match { case JsArray(ar) => ar map { case JsString(s) => s case _ => "" } case _ => Nil }
+              val s_attsKeys = o("attrs") match { case JsArray(ar_p) => getAttributeLabels(ar_p) case _ => Nil }
               val s_attsKey = s_attsKeys match { case s :: _ => Some(s) case Nil => None }
               o("annots") match {
                 case JsArray(arr) =>
@@ -68,7 +86,7 @@ object JsonAnnotationHandler {
                                 // now check for attribute value wildcards
                                 s_attsKeys foreach { a =>
                                   al.hasAttributeWithWildCard(a) match {
-                                    case Some((k,_)) => 
+                                    case Some((k, _)) =>
                                       attC = Some(k)
                                       valC = attVlMap.get(k).getOrElse("--")
                                     case None =>
@@ -76,7 +94,8 @@ object JsonAnnotationHandler {
                                 }
                                 if (justLabel) // when only selecting on the label, add in attribute key and current value
                                   attvls match {
-                                    case a :: Nil => attC = s_attsKey; valC = a
+                                    case a :: Nil =>
+                                      attC = s_attsKey; valC = a
                                     case _ => annotBuf += new Annotation(st, en, false, SLabel(s), vl, None)
                                   }
                                 attC match {
