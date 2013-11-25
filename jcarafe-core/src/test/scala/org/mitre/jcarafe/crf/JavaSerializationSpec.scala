7e5e758e4b0270e1488ae623c347fb52b87d1526
Ben Wellner <wellner@mitre.org>
2013-11-25 10:41:24 -0500

    Updated/refactored test

--- jcarafe-core/src/test/scala/org/mitre/jcarafe/crf/JavaSerializationSpec.scala
+++ jcarafe-core/src/test/scala/org/mitre/jcarafe/crf/JavaSerializationSpec.scala
@@ -4,17 +4,29 @@ import org.scalatest.{FlatSpec}
 
 class JavaSerializationSpec extends FlatSpec {
   
+  def serialize[T](obj: T) = {
+    val fs = new java.io.FileOutputStream("ss")
+    val objOut = new java.io.ObjectOutputStream(fs)
+    objOut.writeObject(obj)
+  }
+  
+  def deserialize[T] : T = {
+    val istr = new java.io.FileInputStream("ss")
+    val objIn = new java.io.ObjectInputStream(istr)
+    objIn.readObject().asInstanceOf[T]        
+  }
+  
   "Basic java serialization test" should "serialize and deserialize String" in {
-      val fs = new java.io.FileOutputStream("ss")
-      val objOut = new java.io.ObjectOutputStream(fs)
-      objOut.writeObject("this is a string")
       
+      serialize("this is a string")
       try {
-        val istr = new java.io.FileInputStream("ss")
-        val objIn = new java.io.ObjectInputStream(istr)
-        val obj = objIn.readObject().asInstanceOf[String]
+        val obj = deserialize[String]
         println("got object back: " + obj)
       } catch { case e: Throwable => e.printStackTrace()}
+  }
+  
+  "MaxEnt classifier serialization test" should "serialize and deserialize MaxEnt trainer object" in {
+    
   }
 
 }
