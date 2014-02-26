1d7066480ad146fb5cdb63531aba208f911c705a
Ben Wellner <wellner@mitre.org>
2014-02-26 14:21:58 -0500

    Bumped v number and added back implicit conversions to Dynamic Feature manager

--- jcarafe-core/src/main/scala/org/mitre/jcarafe/crf/DynamicFeatureManager.scala
+++ jcarafe-core/src/main/scala/org/mitre/jcarafe/crf/DynamicFeatureManager.scala
@@ -31,6 +31,9 @@ class DynamicFeatureManagerBuilder[Obs](
   
   def this(s: String) = this(None, None, None, None, s)
 
+  // implicitly map function objects to FeatureFn objects
+  implicit def upgrad(f: ((Int, SourceSequence[Obs], Int) => FeatureReturn)) : FeatureFn[Obs] = { FeatureFn(f) }
+  
   var ii = 0
   def genSym = {
     val r = "f_"+ii
@@ -99,45 +102,45 @@ class DynamicFeatureManagerBuilder[Obs](
     weightedAttrExpr | distToLeftExpr | distToRightExpr | nodeFnSemiExpr | edgeFnSemiExpr | phraseFnExpr | semiAttributeFnExpr | phraseWdsExpr |
     prefNGramExpr | sufNGramExpr | sentPosExpr | wdLenExpr | wdFnNormExpr | wdPropPrefixFnExpr | posFnExpr 
   
-  def wdFnExpr : Parser[FeatureFn[Obs]]           = "wdFn" ^^ {_ => FeatureFn(wdFn)}
-  def wdFnNormExpr : Parser[FeatureFn[Obs]]       = "wdNormFn" ^^ {_ => FeatureFn(wdFnNorm _)}
-  def caseLessFnExpr : Parser[FeatureFn[Obs]]     = "caselessWdFn" ^^ {_ => FeatureFn(caselessWdFn)}
-  def posFnExpr: Parser[FeatureFn[Obs]]           = "preLabFn" ^^ {_ => FeatureFn(_preLabFn _)}
-  def lexFnExpr : Parser[FeatureFn[Obs]]          = "lexFn" ^^ {_ => FeatureFn(lexFn)}
-  def downLexFnExpr : Parser[FeatureFn[Obs]]      = "downLexFn" ^^ {_ => FeatureFn(downLexFn)}
-  def wdPropFnExpr : Parser[FeatureFn[Obs]]       = "wdPropFn" ^^ {_ => FeatureFn(wordPropertiesFn(false) _) }
-  def wdPropPrefixFnExpr : Parser[FeatureFn[Obs]] = "wdPropPrefixFn(" ~> intVal <~ ")" ^^ {v => FeatureFn(wordPropertiesPrefixesFn(v,false) _) }
-  def downWdPropFnExpr : Parser[FeatureFn[Obs]]   = "downWdPropFn" ^^ {_ => FeatureFn(wordPropertiesFn(true) _) }
-  def wdScoreFnExpr : Parser[FeatureFn[Obs]]      = "wdScoreFn" ^^ {_ => FeatureFn(wordScoresFn _) }
-  def nodeFnExpr : Parser[FeatureFn[Obs]]         = "nodeFn" ^^ {_ => FeatureFn(nodeFn)}
-  def nodeFnSemiExpr : Parser[FeatureFn[Obs]]     = "semiNodeFn" ^^ {_ => FeatureFn(_nodeFnSemi _)}
-  def edgeFnSemiExpr : Parser[FeatureFn[Obs]]     = "semiEdgeFn" ^^ {_ => FeatureFn(_edgeFnSemi _)}
-  def semiAttributeFnExpr: Parser[FeatureFn[Obs]] = "semiAttrFn(" ~> fname <~ ")" ^^ {v => FeatureFn(_phraseAttributeFn(v) _)}
-  def phraseFnExpr : Parser[FeatureFn[Obs]]       = "phraseFn" ^^ {_ => FeatureFn(phraseFn _)}
-  def phraseWdsExpr : Parser[FeatureFn[Obs]]      = "phraseWds" ^^ {_ => FeatureFn(phraseWds _)}
-  def edgeFnExpr : Parser[FeatureFn[Obs]]         = "edgeFn" ^^ {_ => FeatureFn(edgeFn)}
-  def allTagFnExpr : Parser[FeatureFn[Obs]]       = "allTagFn" ^^ {_ => FeatureFn(allTagFn _)}
-  def weightedAttrExpr : Parser[FeatureFn[Obs]]   = "weightedAttrs" ^^ {_ => FeatureFn(weightedAttributes _)}
-  def distToLeftExpr : Parser[FeatureFn[Obs]]     = "distToLeft(" ~> fname ~ "," ~ fname <~ ")" ^^ {case (a~_~v) => FeatureFn(distanceToLeft(a,v) _) }
-  def distToRightExpr : Parser[FeatureFn[Obs]]    = "distToRight(" ~> fname ~ "," ~ fname <~ ")" ^^ {case (a~_~v) => FeatureFn(distanceToRight(a,v) _) }
-  def sentPosExpr : Parser[FeatureFn[Obs]]        = "sentPos" ^^ {_ => FeatureFn(sentPosition _)}
+  def wdFnExpr : Parser[FeatureFn[Obs]]           = "wdFn" ^^ {_ => wdFn}
+  def wdFnNormExpr : Parser[FeatureFn[Obs]]       = "wdNormFn" ^^ {_ => wdFnNorm _}
+  def caseLessFnExpr : Parser[FeatureFn[Obs]]     = "caselessWdFn" ^^ {_ => caselessWdFn}
+  def posFnExpr: Parser[FeatureFn[Obs]]           = "preLabFn" ^^ {_ => _preLabFn _}
+  def lexFnExpr : Parser[FeatureFn[Obs]]          = "lexFn" ^^ {_ => lexFn}
+  def downLexFnExpr : Parser[FeatureFn[Obs]]      = "downLexFn" ^^ {_ => downLexFn}
+  def wdPropFnExpr : Parser[FeatureFn[Obs]]       = "wdPropFn" ^^ {_ => wordPropertiesFn(false) _ }
+  def wdPropPrefixFnExpr : Parser[FeatureFn[Obs]] = "wdPropPrefixFn(" ~> intVal <~ ")" ^^ {v => wordPropertiesPrefixesFn(v,false) _ }
+  def downWdPropFnExpr : Parser[FeatureFn[Obs]]   = "downWdPropFn" ^^ {_ => wordPropertiesFn(true) _ }
+  def wdScoreFnExpr : Parser[FeatureFn[Obs]]      = "wdScoreFn" ^^ {_ => wordScoresFn _ }
+  def nodeFnExpr : Parser[FeatureFn[Obs]]         = "nodeFn" ^^ {_ => nodeFn}
+  def nodeFnSemiExpr : Parser[FeatureFn[Obs]]     = "semiNodeFn" ^^ {_ => _nodeFnSemi _}
+  def edgeFnSemiExpr : Parser[FeatureFn[Obs]]     = "semiEdgeFn" ^^ {_ => _edgeFnSemi _}
+  def semiAttributeFnExpr: Parser[FeatureFn[Obs]] = "semiAttrFn(" ~> fname <~ ")" ^^ {v => _phraseAttributeFn(v) _}
+  def phraseFnExpr : Parser[FeatureFn[Obs]]       = "phraseFn" ^^ {_ => phraseFn _}
+  def phraseWdsExpr : Parser[FeatureFn[Obs]]      = "phraseWds" ^^ {_ => phraseWds _}
+  def edgeFnExpr : Parser[FeatureFn[Obs]]         = "edgeFn" ^^ {_ => edgeFn}
+  def allTagFnExpr : Parser[FeatureFn[Obs]]       = "allTagFn" ^^ {_ => allTagFn _}
+  def weightedAttrExpr : Parser[FeatureFn[Obs]]   = "weightedAttrs" ^^ {_ => weightedAttributes _}
+  def distToLeftExpr : Parser[FeatureFn[Obs]]     = "distToLeft(" ~> fname ~ "," ~ fname <~ ")" ^^ {case (a~_~v) => distanceToLeft(a,v) _ }
+  def distToRightExpr : Parser[FeatureFn[Obs]]    = "distToRight(" ~> fname ~ "," ~ fname <~ ")" ^^ {case (a~_~v) => distanceToRight(a,v) _ }
+  def sentPosExpr : Parser[FeatureFn[Obs]]        = "sentPos" ^^ {_ => sentPosition _}
   
-  def prefFnExpr : Parser[FeatureFn[Obs]]         = "prefixFn(" ~> intVal <~ ")" ^^ { v => FeatureFn(prefixFn(v) _)}
+  def prefFnExpr : Parser[FeatureFn[Obs]]         = "prefixFn(" ~> intVal <~ ")" ^^ { v => prefixFn(v) _}
 
-  def attributeFnExpr : Parser[FeatureFn[Obs]]    = "attributeFn(" ~> fname <~ ")" ^^ {v => FeatureFn(attributeFn(v) _)}
-  def sufFnExpr : Parser[FeatureFn[Obs]]          = "suffixFn(" ~> intVal <~ ")" ^^ { v => FeatureFn(suffixFn(v) _)}
-  def antiPrefFnExpr : Parser[FeatureFn[Obs]]     = "antiPrefixFn(" ~> intVal <~ ")" ^^ { v => FeatureFn(antiPrefixFn(v) _)}
-  def antiSufFnExpr : Parser[FeatureFn[Obs]]      = "antiSuffixFn(" ~> intVal <~ ")" ^^ { v => FeatureFn(antiSuffixFn(v) _)}
-  def prefNGramExpr : Parser[FeatureFn[Obs]]      = "prefNGrams(" ~> intVal ~ "," ~ intVal <~ ")" ^^ {case (s~_~r) => FeatureFn(prefNgrams(s,r) _) }
-  def sufNGramExpr : Parser[FeatureFn[Obs]]       = "sufNGrams(" ~> intVal ~ "," ~ intVal <~ ")" ^^ {case (s~_~r) => FeatureFn(sufNgrams(s,r) _) }
-  def wdLenExpr : Parser[FeatureFn[Obs]]          = "wdLen" ^^ {_ => FeatureFn(wdLen _)}
+  def attributeFnExpr : Parser[FeatureFn[Obs]]    = "attributeFn(" ~> fname <~ ")" ^^ {v => attributeFn(v) _}
+  def sufFnExpr : Parser[FeatureFn[Obs]]          = "suffixFn(" ~> intVal <~ ")" ^^ { v => suffixFn(v) _}
+  def antiPrefFnExpr : Parser[FeatureFn[Obs]]     = "antiPrefixFn(" ~> intVal <~ ")" ^^ { v => antiPrefixFn(v) _}
+  def antiSufFnExpr : Parser[FeatureFn[Obs]]      = "antiSuffixFn(" ~> intVal <~ ")" ^^ { v => antiSuffixFn(v) _}
+  def prefNGramExpr : Parser[FeatureFn[Obs]]      = "prefNGrams(" ~> intVal ~ "," ~ intVal <~ ")" ^^ {case (s~_~r) => prefNgrams(s,r) _ }
+  def sufNGramExpr : Parser[FeatureFn[Obs]]       = "sufNGrams(" ~> intVal ~ "," ~ intVal <~ ")" ^^ {case (s~_~r) => sufNgrams(s,r) _ }
+  def wdLenExpr : Parser[FeatureFn[Obs]]          = "wdLen" ^^ {_ => wdLen _}
 
   def regexpFnExpr : Parser[FeatureFn[Obs]]       = "regexpFn(" ~> fname ~ "," ~ """(\\\)|[^\)])+""".r <~ ")" ^^ 
                                             {case (fn~_~e) => 
 					      val parenMap = """\\\)""".r // use to map escaped parens to actual parens
 					      val ne = parenMap.replaceAllIn(e,{_ => ")"})
 					      FeatureFn(_regexpFn(fn,ne.r) _)}
-  def predicateExpr : Parser[FeatureFn[Obs]]      = "predicateFn(" ~ fname ~ "," ~ repsep(simpleFnExpr,",") ~ ")" ^^ {case (_~fn~_~fs~_) => FeatureFn(predicateFn(fn,fs) _)}
+  def predicateExpr : Parser[FeatureFn[Obs]]      = "predicateFn(" ~ fname ~ "," ~ repsep(simpleFnExpr,",") ~ ")" ^^ {case (_~fn~_~fs~_) => predicateFn(fn,fs) _}
   
   def rangeExpr : Parser[Seq[Int]] = basRangeExpr | toRangeExpr
   def basRangeExpr : Parser[Seq[Int]] = "(" ~> repsep(intVal,",") <~ ")" ^^ {_.toSeq}
